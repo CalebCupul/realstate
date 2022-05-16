0cbfe356b10caaeca3769dbb8f6de739b4ee2788
Caleb <caleb.cupul@alumnos.udg.mx>
2022-05-16 00:00:06 -0500

    production env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Intervention\Image\Image;
 use Optix\Media\Facades\Conversion;
@@ -26,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         //
         Paginator::useBootstrap();
 
